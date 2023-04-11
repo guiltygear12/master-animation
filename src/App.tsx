8949bf53af343806f4bf5b8875ba163c697b5b4a
qkralsgud9 <mylove700757@gmail.com>
2023-04-11 15:24:09 +0900

    마스터클래스8-framer animation 셋업및 이해

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { type } from "os";
+
+const Wrapper = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const Box = styled(motion.div)`
+    width: 200px;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    border-radius: 16px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Wrapper>
+            <Box
+                transition={{
+                    type: "spring",
+                    delay: 1,
+                }}
+                initial={{ scale: 0, rotateZ: 180 }}
+                animate={{ scale: 1, rotateZ: 0 }}
+            >
+                12
+            </Box>
+        </Wrapper>
+    );
 }
 
 export default App;
