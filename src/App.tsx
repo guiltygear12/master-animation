c6f7e8ae95017543e8c692197398988c8c1e5c7e
qkralsgud9 <mylove700757@gmail.com>
2023-04-12 21:42:39 +0900

    마스터클래스8- animate presence

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,63 @@
 import styled from "styled-components";
-import {
-    animate,
-    motion,
-    useMotionValue,
-    useMotionValueEvent,
-    useScroll,
-    useTransform,
-    useViewportScroll,
-} from "framer-motion";
-import { useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
     height: 100vh;
     width: 100vw;
     display: flex;
+    flex-direction: column;
+    gap: 8px;
     justify-content: center;
     align-items: center;
     background: linear-gradient(135deg, #e09, #d0e); ;
 `;
-const Svg = styled.svg`
-    width: 100px;
-    color: white;
-    path {
-        stroke: white;
-        stroke-width: 4;
-    }
+
+const Box = styled(motion.div)`
+    width: 200px;
+    height: 100px;
+    border-radius: 10px;
+    background-color: white;
 `;
-const stageSvg = {
+const boxVariants = {
     start: {
-        pathLength: 0.1,
-        fill: "rgba(255,255,255,0)",
-        opacity: 1,
+        opacity: 0,
+        scale: 0,
     },
     end: {
-        pathLength: 1,
-        fill: "rgba(255,255,255,1)",
         opacity: 1,
+        scale: 1,
+        transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 1,
+        },
+    },
+    leaving: {
+        opacity: 0,
+        scale: 0,
+        rotateZ: 360,
+        transition: {
+            duration: 0.5,
+        },
     },
 };
 function App() {
+    const [showing, setShowing] = useState(false);
+    const toggleShowing = () => setShowing((prev) => !prev);
     return (
         <Wrapper>
-            <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                <motion.path
-                    variants={stageSvg}
-                    initial="start"
-                    animate="end"
-                    fill="transparent"
-                    transition={{
-                        default: {
-                            duration: 3,
-                        },
-                        fill: { duration: 2, delay: 1 },
-                    }}
-                    d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z"
-                />
-            </Svg>
+            <AnimatePresence>
+                {showing ? (
+                    <Box
+                        variants={boxVariants}
+                        initial="start"
+                        animate="end"
+                        exit="leaving"
+                    />
+                ) : null}
+            </AnimatePresence>
+            <button onClick={toggleShowing}> 온오프 </button>
         </Wrapper>
     );
 }
