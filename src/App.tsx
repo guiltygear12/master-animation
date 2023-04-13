8623a03d8ba04abfa8eafb897bd662b66a92515e
qkralsgud9 <mylove700757@gmail.com>
2023-04-13 20:59:29 +0900

    마스터클래스8 - layoutId

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,16 @@ const Wrapper = styled(motion.div)`
     height: 100vh;
     width: 100vw;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     justify-content: center;
     align-items: center;
+    gap: 16px;
     background: linear-gradient(135deg, #e09, #d0e); ;
 `;
 
 const Box = styled(motion.div)`
-    position: absolute;
-    top: 100px;
     width: 200px;
-    height: 100px;
+    height: 200px;
     border-radius: 10px;
     background-color: white;
     display: flex;
@@ -26,66 +25,41 @@ const Box = styled(motion.div)`
     font-weight: 600;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.3);
 `;
-
-const boxVariant = {
-    entry: (back: boolean) => ({
-        x: back ? -500 : 500,
-        opacity: 0,
-        scale: 0,
-    }),
-    center: {
-        opacity: 1,
-        scale: 1,
-        x: 0,
-        transition: {
-            duration: 0.3,
-        },
-    },
-    exit: (back: boolean) => ({
-        x: back ? 500 : -500,
-        opacity: 0,
-        scale: 0,
-        transition: {
-            duration: 0.3,
-        },
-    }),
-};
+const Circle = styled(motion.div)`
+    background-color: #00a5ff;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.3);
+`;
 
 function App() {
-    const [visible, setVisible] = useState(1);
-    const [back, setBack] = useState(false);
-    const next = () => {
-        setBack(false);
-        setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+    const [clicked, setClicked] = useState(0);
+    const toggleClicked = () => {
+        clicked === 3 ? setClicked(0) : setClicked((prev) => prev + 1);
     };
-    const prev = () => {
-        setBack(true);
-        setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-    };
-    const autoSlide = () => {
-        setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-    };
-    useEffect(() => {
-        // const interval = setInterval(() => {
-        //     autoSlide();
-        // }, 3000);
-    }, []);
     return (
-        <Wrapper>
-            <AnimatePresence mode="wait" custom={back}>
-                <Box
-                    custom={back}
-                    variants={boxVariant}
-                    initial="entry"
-                    animate="center"
-                    exit="exit"
-                    key={visible}
-                >
-                    {visible}
-                </Box>
-            </AnimatePresence>
-            <button onClick={prev}>Prev</button>
-            <button onClick={next}>Next</button>
+        <Wrapper onClick={toggleClicked}>
+            <Box>
+                {clicked === 0 ? (
+                    <Circle layoutId="circle" style={{ borderRadius: "50%" }} />
+                ) : null}
+            </Box>
+            <Box>
+                {clicked === 1 ? (
+                    <Circle layoutId="circle" style={{ borderRadius: 0 }} />
+                ) : null}
+            </Box>
+            <Box>
+                {clicked === 2 ? (
+                    <Circle layoutId="circle" style={{ borderRadius: 0 }} />
+                ) : null}
+            </Box>
+            <Box>
+                {clicked === 3 ? (
+                    <Circle layoutId="circle" style={{ borderRadius: 0 }} />
+                ) : null}
+            </Box>
         </Wrapper>
     );
 }
